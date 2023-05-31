bee9f59f390a7ecaec76ad760318afb78c294560
CnL <67447796+Fra8k@users.noreply.github.com>
2023-05-31 18:31:03 +0400

    Room.js

---
+++ Room.js
@@ -0,0 +1,42 @@
+$(`<style>.nosel.d-flex.room.r18863f0342ax48280dbx-q3b03k .label-primary {background-color: transparent !important;etter-spacing: 1px;-webkit-text-fill-color: #ffffff;padding: 0px !important;border-radius: 0px 15px 15px 15px !important;
+margin-top: 30px!important;}
+
+.nosel.d-flex.room.r18863f0342ax48280dbx-q3b03k .corner.fa.fa-user.label.uc.fa-microphone.label-danger {background-color: transparent;letter-spacing: 1px;-webkit-text-fill-color: #ffffff;padding: 0px !important;border-radius: 50px !important;background-color: #transparent;
+margin-top: 30px!important;}
+
+
+    .nosel.d-flex.room.r18863f0342ax48280dbx-q3b03k.bord {border-inline: 4px solid #f7f4f2 !important;}
+    .nosel.d-flex.room.r18863f0342ax48280dbx-q3b03k {
+    border-radius: 0px 10px 10px 10px;
+    display: inline-block;
+    background-image: url(https://up6.cc/2023/04/16825749970691.jpg);
+	background-size: 100%;
+	padding: 5px !important;
+    }
+    .nosel.d-flex.room.r18863f0342ax48280dbx-q3b03k .dots.mini.u-msg {
+    text-align: center;
+    -webkit-text-fill-color: transparent;
+    -webkit-background-clip: text;-webkit-text-fill-color: transparent;animation: textclip 1s linear infinite;display: inline-block;
+    }
+    .nosel.d-flex.room.r18863f0342ax48280dbx-q3b03k .fitimg.u-pic.borderg{background-size: 0% 0%;-webkit-text-fill-color: white;
+	
+	height: 0px !important;
+	width : 0px !important;
+	padding: 5px !important;
+	background-color: #transparent !important;
+	background-color: transparent !important;
+	border: 2px solid transparent !important;}
+    .nosel.d-flex.room.r18863f0342ax48280dbx-q3b03k .u-topic.dots {
+    text-transform: uppercase;
+    -webkit-text-fill-color: transparent;
+    animation: textclip 2s linear infinite;
+    display: inline-block;
+    font-size: 20px !important;
+    font-style: normal;
+    font-family: sans-serif;
+	
+    color: transparent !important;
+    background-size: 100% 100%;
+    margin-top: -2px;
+    margin-left: 5px;}
+    @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
